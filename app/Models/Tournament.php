6b6d14e3da2a904c52127b5abb3915c39dca918f
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-06-14 16:32:46 +0300

    Update Tournament.php

--- app/Models/Tournament.php
+++ app/Models/Tournament.php
@@ -1,12 +1,18 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Tournament extends Model
 {
     protected $fillable = [
-      'title','description','prize_pool',
-      'competition_type','tournament_type','location'
+        'title',
+        'description',
+        'prize_pool',
+        'competition_type',
+        'tournament_type',
+        'location',
     ];
 
     public function players()
@@ -23,8 +29,9 @@ class Tournament extends Model
     {
         return $this->hasMany(Source::class);
     }
+
     public function comments()
     {
-        return $this->hasmany(Comment::class);
+        return $this->hasMany(Comment::class);
     }
 }
