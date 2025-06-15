739fd2f1276dd56c9d124e681bafec1d49b96c66
EBRZ124 <130497524+EBRZ124@users.noreply.github.com>
2025-06-15 23:06:23 +0300

    Update contact.blade.php
    Removed contact boxes.

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -14,31 +14,4 @@
     </ul>
   </section>
 
-  <section class="box post">
-    <header>
-      <h3>Send a Message</h3>
-    </header>
-    <form method="POST" action="{{ route('contact.send') }}">
-      @csrf
-
-      <div class="field half">
-        <label for="name">Your Name</label>
-        <input type="text" name="name" id="name" placeholder="White Cat" required />
-      </div>
-
-      <div class="field half">
-        <label for="email">Your Email</label>
-        <input type="email" name="email" id="email" placeholder="john@example.com" required />
-      </div>
-
-      <div class="field">
-        <label for="message">Message</label>
-        <textarea name="message" id="message" rows="6" placeholder="Write your message here…" required></textarea>
-      </div>
-
-      <ul class="actions">
-        <li><input type="submit" value="Send Message" class="button" /></li>
-      </ul>
-    </form>
-  </section>
 @endsection
